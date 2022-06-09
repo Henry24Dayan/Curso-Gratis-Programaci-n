36cdc3cfbdf3cf0f2e69354ad26ce7fa029506eb
Henry24Dayan <32483743+Henry24Dayan@users.noreply.github.com>
2022-06-08 21:12:49 -0500

    Add files via upload
    Conociendo objetos y sus metodos y atributos. Haciendo pruebas con el objeto Windows y su metodo .location mostrando en pantalla la ruta.  Adicional creamos archivo js y lo referenciamos en el htmll con la ettiqueta script y su metodo src.

---
+++ Ejercicio Objetos/DOM/codigo.js
@@ -0,0 +1,6 @@
+
+
+
+var ruta =window.location;                                          // Definimos variable y le asignamos el valor del objeto y su método. 
+console.log(console);          //Deja mesajes en la consola, El objeto es console, el metodo es log.
+document.write("Holaa tu ruta actual en la página es: " + ruta);   // Document es el objeto y .write es el metodo o funcion del objeto lo que va en los parentesis son los parametros. 
