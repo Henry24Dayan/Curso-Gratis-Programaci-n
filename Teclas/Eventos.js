f3b86c74f9ac30abc9c1f64f299353bfed5d7358
Henry24Dayan <32483743+Henry24Dayan@users.noreply.github.com>
2022-08-22 11:38:43 -0500

    Add files via upload

--- Teclas/Eventos.js
+++ Teclas/Eventos.js
@@ -1,15 +1,33 @@
     var teclas = {      //Definimos un objeto literal
 
-    UP : 38,            //Variables    
-    DOWN : 40,
-    LEFT : 37,
-    RIGHT : 39
+        UP : 38,            //Variables    
+        DOWN : 40,
+        LEFT : 37,
+        RIGHT : 39
 
+
+                }
+
+                
+document.addEventListener("keyup", dibujarTeclado); //Key Up es la funcion del Listener y el lanzador que ejecuta la funcion
+var c = document.getElementById("area1");
+var papel = c.getContext("2d"); 
+
+dibujarlinea("blue",100,100,200,200,papel);
+
+
+function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)  //Declaramos las variables como si pusieramos un var; Se declara cada vez que la función es invocada.
+
+{
+//Debemos cambiar los valores fijos por los valores variables que vienen del parámetro//
+lienzo.beginPath();                 //Definimos la funcion del canvas para inicicar a dibujar.
+lienzo.strokeStyle = color ;        // Asignamos el valor que recibe por parámetro.
+lienzo.moveTo(xinicial,xfinal)      // Asignamos al función el valor que recibe por parámetro.
+lienzo.lineTo(yinicial,yfinal)    //Asignamos el valor que recibe por parámetro.
+lienzo.stroke();                    //Dibuja la línea.
+lienzo.closePath();                 //Método para finalizar el dibujo. Cerrar el trazo  (Levantar el lapiz)
 
 }
-
-
-document.addEventListener("keyup", dibujarTeclado) //Key Up es la funcion del Listener y el lanzador que ejecuta la funcion
 
 function dibujarTeclado(evento)  //Definimos la funcion y ponemos el parametro que almacenara todos los eventos
 
