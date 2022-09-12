389e23bc94ded505303320088d3f602471c1aafe
Henry24Dayan <32483743+Henry24Dayan@users.noreply.github.com>
2022-09-11 19:14:56 -0500

    Add files via upload
    Final documenst: En esta version solicitamos el numero de lineas a dibujar al usuario. 
    Definimos las variables y usamos por medio del id las caracteristicas del objeto y trabajarlas en javascritp.
    br para saltos de linea.
    utf nos permite caracteres especiales.
    input texto y boton o diferentes tipos de datos del formulario.
    value es el valor del boton.
    Como usar eventos con AddEventlistener y definir objetos para usar  eventos especificos.

--- Canvas dibujo/lienzp.js
+++ Canvas dibujo/lienzp.js
@@ -1,25 +1,13 @@
 
-var d = document.getElementById("Dibujo") ;  //En javascript  getEllementByiD   es el  método de document. que nos trae  una de las ramas de html
+
+var texto = document.getElementById("Texto_Lineas");     //En javascript  getEllementByiD   es el  método de document. que nos trae  una de las ramas de html
+var boton = document.getElementById("botoncito"); //En javascript  getEllementByiD   es el  método de document. que nos trae  una de las ramas de html
+
+boton.addEventListener("click", dibujoPorClick ) //Aca definimos el evento que se disparara al dar click
+
+var d = document.getElementById("Dibujo");  //En javascript  getEllementByiD   es el  método de document. que nos trae  una de las ramas de html
+var ancho = d.width;                            //Obtenemos el ancho del canvas
 var lienzo = d.getContext("2d");               //Definimos si es en 2D o 3D el contexto; getContext es una funcion de canvas que me permite obtener el área donde voy a diibujar.
-var lineas = 30;    //Definimos el límite de raayas
-var l = 0;            //Ciclo//contador 
-var yi, xf;          //Definimos dos variables vacias.
-
-//Para tener un ciclo debemos tener unn límite para que no sea infinito. //
-for(l = 0; l < lineas; l++)  //Este ciclo see  ejecutara hasta que esto se cumpla.
-
-{
-
-    yi = 10  * l; // Creamos la  operación que se ejecutaaraa.
-    xf = 10  * (l + 1);// Creamos la  operación que se ejecutaaraa.
-
-    dibujarlinea("blue",0,yi,xf,300);  //Invocamos la función para que se ejecute.
-
-     l++;  // Le sumamos 1 cada vez que se dibuje una linea 
-}
-
-
-
 
 
 function dibujarlinea(color, xinicial, yinicial, xfinal, yfinal)  //Declaramos las variables como si pusieramos un var; Se declara cada vez que la función es invocada.
@@ -33,6 +21,30 @@ lienzo.lineTo(yinicial,yfinal)    //Asignamos el valor que recibe por parámetro
 lienzo.stroke();                    //Dibuja la línea.
 lienzo.closePath();                 //Método para finalizar el dibujo. Cerrar el trazo  (Levantar el lapiz)
 
+} 
+
+function dibujoPorClick()
+{
+var lineas= parseInt(texto.value);
+var espacio = ancho / lineas;
+var l = 0;            //Ciclo//contador 
+var yi, xf;          //Definimos dos variables vacias.
+var colorcito  = "#AAF";
+//Para tener un ciclo debemos tener unn límite para que no sea infinito. //
+for(l = 0; l < lineas; l++)  //Este ciclo see  ejecutara hasta que esto se cumpla.
+
+{
+
+    
+    yi = espacio  * l; // Creamos la  operación que se ejecutaaraa.
+    xf = espacio * (l + 1);// Creamos la  operación que se ejecutaaraa.
+
+    dibujarlinea(colorcito,0,yi,xf,300);  //Invocamos la función para que se ejecute.
+
+     l++;  // Le sumamos 1 cada vez que se dibuje una linea 
 }
 
- 
+    
+
+
+}
