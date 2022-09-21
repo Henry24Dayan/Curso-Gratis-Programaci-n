29a4e9a0737eeeb59b753550818bf435fb0363a6
Henry24Dayan <32483743+Henry24Dayan@users.noreply.github.com>
2022-09-21 12:54:59 -0500

    Add files via upload
    Mejora en la posicion de imagenes para que no se super pongan encima una de otra.

--- VillaPlatzi/platzi.js
+++ VillaPlatzi/platzi.js
@@ -17,7 +17,7 @@ var vaca = {
 
 };
 
-var pollo = {
+ var pollo = {
 
     url: "pollo.png",                            //Direccion  del directorio de la imagen
     cargaOK: false                              
@@ -73,38 +73,44 @@ function dibujar()                               //Definimos la funcion que trae
 {
     if(fondo.cargaOK == true)
     {
-        papel.drawImage(fondo.imagen, 0, 0)                 //Donde papel es el canvas y traemos una funcion de Image que trae la foto de 500x500 y 0,0 donde inicia      
+        papel.drawImage(fondo.imagen, 0, 0)                 //Donde usamos una funcion donde el parametro es el objeto completo de imagen y la posicvion en x y y del canvas    
 
     } 
     if(vaca.cargaOK == true)
     {
-        for( var v=0; v<5; v++)                                   //Super poder for para dibujar mas vacas.
+        for( var v=0; v<3; v++)                                   //Super poder for para dibujar mas vacas.
         
         {
-            var x = aleatorio(0,420);                           //Definimos variable para ejecutar la funcion aleatorio y le mandamos los parametros o rangos.
-            var y = aleatorio(0,420);
+            var x = aleatorio(0, 6);                           //Definimos variable para ejecutar la funcion aleatorio y le mandamos los parametros o rangos.
+            var y = aleatorio(0, 6);
+            x = x * 60;                                        //Hacemos la ecuacion para que no se mezclen las vacas: 500(canvas) / 80(pixeles imagenes) nos da 6 entonces definimos un numero aleatorio entre 0 y 6 o un numero cercano para multiplicarlo por 80 y asi que se ordenen.   
+            y = y * 60;
             papel.drawImage(vaca.imagen, x, y)                 //Donde papel es el canvas y traemos una funcion de Image que trae la foto de 500x500 y 0,0 donde inicia      
         }
         
     }
     if(pollo.cargaOK == true)
     {
-        for( var p=0; p<5; p++)                                   //Super poder for para dibujar mas vacas.
+        for( var p=0; p<1; p++)                                   //Super poder for para dibujar mas vacas.
         
         {
-            var x = aleatorio(0,420);                           //Definimos variable para ejecutar la funcion aleatorio y le mandamos los parametros o rangos.
-            var y = aleatorio(0,420);
+            var x = aleatorio(0, 6);                           //Definimos variable para ejecutar la funcion aleatorio y le mandamos los parametros o rangos.
+            var y = aleatorio(0, 6);
+            x = x * 60;                                        //Hacemos la ecuacion para que no se mezclen las vacas: 500(canvas) / 80(pixeles imagenes) nos da 6 entonces definimos un numero aleatorio entre 0 y 6 o un numero cercano para multiplicarlo por 80 y asi que se ordenen.   
+            y = y * 60;
             papel.drawImage(pollo.imagen, x, y)                 //Donde papel es el canvas y traemos una funcion de Image que trae la foto de 500x500 y 0,0 donde inicia      
         }
         
     }
     if(cerdo.cargaOK)                                              // Si es falso no se ejecuta al if solo le importa lo que recibe
     {
-        for( var c=0; c<8; c++)                                   //Super poder for para dibujar mas vacas.
+        for( var c=0; c<5; c++)                                   //Super poder for para dibujar mas vacas.
         
         {
-            var x = aleatorio(0,420);                           //Definimos variable para ejecutar la funcion aleatorio y le mandamos los parametros o rangos.
-            var y = aleatorio(0,420);
+            var x = aleatorio(0, 6);                           //Definimos variable para ejecutar la funcion aleatorio y le mandamos los parametros o rangos.
+            var y = aleatorio(0, 6);
+            x = x * 60;                                        //Hacemos la ecuacion para que no se mezclen las vacas: 500(canvas) / 80(pixeles imagenes) nos da 6 entonces definimos un numero aleatorio entre 0 y 6 o un numero cercano para multiplicarlo por 80 y asi que se ordenen.   
+            y = y * 60;
             papel.drawImage(cerdo.imagen, x, y)                 //Donde papel es el canvas y traemos una funcion de Image que trae la foto de 500x500 y 0,0 donde inicia      
         }
         
