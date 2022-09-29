15b5c84a211fd9b16e99df9bbad118f1f367012f
Henry24Dayan <32483743+Henry24Dayan@users.noreply.github.com>
2022-09-28 19:47:52 -0500

    Add files via upload
    Otro ejemplo del problema: "Escribe los números del 1 al 100 pero si el número es múltiplo de 3 escribe Fizz si el número es multiplo de 5 escribe buzz y si es múltiplo de 3 y 5 escribe FizzBuzz." 
    Usando una funcion y multiples operadores.

--- Modulo/fizz.js
+++ Modulo/fizz.js
@@ -1,17 +1,17 @@
 var numeros = 100;                              //Variable límite de números.
-                       
+var divisible = false;                          //Inicia en false;                       
 
 for ( var i = 1; i <=numeros; i++){             //Ciclo For como vemos pomos que sea menor o igual a la variable números    
                           
-    if ( i % 3 == 0){                           //MÓDULO - Sirve para saber el residuo de una división (Cuando el número no es divisible)
+    if (esDivisible(i,3)){                           //MÓDULO - Sirve para saber el residuo de una división, tambien sirve para saber pares e impares por emdio del residuo de la divion.
 
         document.write("Fizz");               //Si es multiplo de 3 escribira   
        
     } 
-    if( i % 5 == 0){                     // si -->          
+     if(esDivisible(i,5)){                     // si -->          
         document.write("Buzz");               //Si es multiplo de 5 escribira     
     }  
-     if(i % 3 != 0 && i % 5 != 0)        //Usando multiples operadores, (Si el modulo de 3es diferente a 0 y el modulo de 5 es 0 se ejecutara) , Funciona como una Tabla de verdad
+     if(!esDivisible(i,3) && !esDivisible(i,5))        //Usando multiples operadores, (Si el modulo de es diferente(!) a 0 y el modulo de 5 es diferente a 0 se ejecutara) , Funciona como una Tabla de verdad
      {                              //Sino escribira i
         
         document.write(i);                   
@@ -19,5 +19,19 @@ for ( var i = 1; i <=numeros; i++){             //Ciclo For como vemos pomos que
      }
     document.write( "<br/> ");              //Ponemos este document dentro del ciclo antes de iterar el ciclo pero despues de las condiciones y asi la comparten ambas. Para que ejecute el salto de linea,  Concatenamos etiqueta html br para salto de linea sencillo.
     
+    function esDivisible (num, divisor){        //Creamos una funcion donde si es Divisible retorna verddero y si no falso.
+
+        if(num % divisor == 0){
+
+                return true;
+        
+        }  else{
+            return false;
+        }    
+
+
+
+    }
+
     
 }
