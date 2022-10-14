92d58773d70cd111cceb0cc58f1ab9a509258261
Henry24Dayan <32483743+Henry24Dayan@users.noreply.github.com>
2022-10-13 20:51:52 -0500

    Add files via upload
    Final: Creando Array associativo(Diccionario), recorriendo con el ciclo for Array con In y Of para mostrar el objeto y ejecutar su atributo.

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -3,15 +3,16 @@ imagenes ["Cauchin"] = "vaca.png";        //Asignamos al nombre una imagen
 imagenes ["Pokacho"] = "pollo.png";
 imagenes ["Tocinauro"] = "cerdo.png";
 
-var cauchin = new Pakiman("Cauchin", 100, 30);              //Objeto que vienen de la misma instancia(Clase)
-var Pokacho = new Pakiman("Pokacho", 80, 50);               //Objeto que vienen de la misma instancia(Clase)
-var tocinauro = new Pakiman("Tocinauro", 120, 40);               //Objeto que vienen de la misma instancia(Clase)
-
-console.log(cauchin,Pokacho,tocinauro);                     //Podemos pasar varios parametros.
-
-
+//Crearemos una coleccion de pakimanes en cada posición del Array sin definir las variables
+var coleccion = [];                                     //Definimos el array.
+coleccion.push(new Pakiman("Tocinauro", 120, 40));      //Invocamos la clase sin necesidad de una variable, 
+coleccion.push(new Pakiman("Pokacho", 80, 50));         //directamente la isntanciamos y queda en cada sub[] del array
+coleccion.push(new Pakiman("Cauchin", 100, 30));
 
 
-Pokacho.mostrar();                                          //Llamamos la función mostrar para cada clase.
-tocinauro.mostrar();
-cauchin.mostrar();
+for (var pakitos of coleccion)    //Of es un Ciclo que permite recorrer por la cantidad de objetos que esta dentro del array y lo colocara en la variable (Muestra la instancia de la clase)
+{                               
+
+    pakitos.mostrar();              // Esto Iterara las veces que existe el objeto dentro del array y ejecutara cada objeto y su función:
+    console.log(pakitos)            // nos muestra en la consola directamente la instancia del objeto
+}
